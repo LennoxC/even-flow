a3300cec1625afe4f0799e3505f491aed341ceb8
Lennox Crowe <lennox.crowe@earthsciences.nz>
2026-07-28 17:01:51 +1200

    layer and batch norm WIP

--- src/even_flow/module/autoencoder/Autoencoder.py
+++ src/even_flow/module/autoencoder/Autoencoder.py
@@ -2,6 +2,7 @@ import torch
 from abc import ABC, abstractmethod
 from even_flow.config.AutoencoderConfig import AutoencoderConfig, ConvolutionalAutoencoderConfig
 from even_flow.module.conv.ConvLayer import ConvUpsampleLayer, ConvDownsampleLayer
+from even_flow.config.AutoencoderConfig import UpsampleLayerConfig
 
 class AutoencoderBase(torch.nn.Module):
     """
@@ -61,35 +62,42 @@ class ConvolutionalAutoencoder(AutoencoderBase):
                                               kernel_size=layer_config.kernel_size,
                                               activation=activation,
                                               downsample_method=layer_config.downsample_method))
+
+            norm = self._norm(layer_config)
+            if norm is not None:
+                layers.append(norm)
         
         return torch.nn.Sequential(*layers)
 
     def build_decoder(self):
         layers = []
-        if self.config.decoder_layers is not None:
-            for layer_config in self.config.decoder_layers:
 
-                activation = layer_config.activation if layer_config.activation is not None else self.config.activation
+        configs = self.config.decoder_layers if self.config.decoder_layers is not None else reversed(self.config.encoder_layers)
 
-                layers.append(ConvUpsampleLayer(dim=layer_config.dim,
-                                                in_channels=layer_config.in_channels,
-                                                out_channels=layer_config.out_channels,
-                                                kernel_size=layer_config.kernel_size,
-                                                activation=activation,
-                                                upsample_method=layer_config.upsample_method))
-        else:
-            # If decoder_layers is None, create a reverse of the encoder with nearest upsampling
-            for layer_config in reversed(self.config.encoder_layers):
-                
-                activation = layer_config.activation if layer_config.activation is not None else self.config.activation
-                
-                layers.append(ConvUpsampleLayer(dim=layer_config.dim,
-                                                in_channels=layer_config.out_channels,
-                                                out_channels=layer_config.in_channels,
-                                                kernel_size=layer_config.kernel_size,
-                                                activation=activation,
-                                                upsample_method="nearest"))
+        for layer_config in configs:
+            activation = layer_config.activation if layer_config.activation is not None else self.config.activation
+            upsample_method = layer_config.upsample_method if isinstance(layer_config, UpsampleLayerConfig) else "nearest"
+
+            layers.append(ConvUpsampleLayer(dim=layer_config.dim,
+                                            in_channels=layer_config.out_channels,
+                                            out_channels=layer_config.in_channels,
+                                            kernel_size=layer_config.kernel_size,
+                                            activation=activation,
+                                            upsample_method=upsample_method))
+            
+            norm = self._norm(layer_config)
+            if norm is not None:
+                layers.append(norm)
         
         return torch.nn.Sequential(*layers)
+
+    def _norm(self, layer_config):
+        if self.config.layer_norm:
+            
+            return torch.nn.LayerNorm([layer_config.out_channels, ])
+        elif self.config.batch_norm:
+            return getattr(torch.nn, f"BatchNorm{layer_config.dim}d")(layer_config.out_channels)
+        else:
+            return None
 
     
--- src/even_flow/tests/test_autoencoder.py
+++ src/even_flow/tests/test_autoencoder.py
@@ -18,7 +18,39 @@ CONFIGS = [
         ],
         activation="GELU",
         layer_norm=False,
-        batch_norm=False)
+        batch_norm=False),
+    
+    ConvolutionalAutoencoderConfig(
+        input_dim=(3, 128, 128),
+        encoder_layers=[
+            DownsampleLayerConfig(dim=2, in_channels=3, out_channels=16, kernel_size=3, activation="GELU", downsample_method="max"),
+            DownsampleLayerConfig(dim=2, in_channels=16, out_channels=32, kernel_size=3, activation="GELU", downsample_method="avg"),
+            DownsampleLayerConfig(dim=2, in_channels=32, out_channels=64, kernel_size=3, activation="GELU", downsample_method="max"),
+        ],
+        decoder_layers=[
+            UpsampleLayerConfig(dim=2, in_channels=64, out_channels=32, kernel_size=3, activation="GELU", upsample_method="nearest"),
+            UpsampleLayerConfig(dim=2, in_channels=32, out_channels=16, kernel_size=3, activation="GELU", upsample_method="bilinear"),
+            UpsampleLayerConfig(dim=2, in_channels=16, out_channels=3, kernel_size=3, activation="Sigmoid", upsample_method="nearest"),
+        ],
+        activation="GELU",
+        layer_norm=True,
+        batch_norm=False),
+    
+    ConvolutionalAutoencoderConfig(
+        input_dim=(3, 128, 128),
+        encoder_layers=[
+            DownsampleLayerConfig(dim=2, in_channels=3, out_channels=16, kernel_size=3, activation="GELU", downsample_method="max"),
+            DownsampleLayerConfig(dim=2, in_channels=16, out_channels=32, kernel_size=3, activation="GELU", downsample_method="avg"),
+            DownsampleLayerConfig(dim=2, in_channels=32, out_channels=64, kernel_size=3, activation="GELU", downsample_method="max"),
+        ],
+        decoder_layers=[
+            UpsampleLayerConfig(dim=2, in_channels=64, out_channels=32, kernel_size=3, activation="GELU", upsample_method="nearest"),
+            UpsampleLayerConfig(dim=2, in_channels=32, out_channels=16, kernel_size=3, activation="GELU", upsample_method="bilinear"),
+            UpsampleLayerConfig(dim=2, in_channels=16, out_channels=3, kernel_size=3, activation="Sigmoid", upsample_method="nearest"),
+        ],
+        activation="GELU",
+        layer_norm=False,
+        batch_norm=True)
 ]
 
 @pytest.mark.parametrize("model_config", CONFIGS)
